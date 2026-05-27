03c7a327fa1554dafaaf311e6d93996238845539
jagadeesh <jagadeesh@qtsolv.com>
2026-05-27 22:33:55 +0530

    added page objects

--- support/fixtures.ts
+++ support/fixtures.ts
@@ -1,1 +1,44 @@
-export * from "@automation-ai/web-support/fixtures";
+import { test as base, expect } from "@playwright/test";
+import { CartPage } from "../pageobjects/CartPage";
+import { CheckoutCompletePage } from "../pageobjects/CheckoutCompletePage";
+import { LoginPage } from "../pageobjects/LoginPage";
+import { OverviewPage } from "../pageobjects/OverviewPage";
+import { ProductDetailPage } from "../pageobjects/ProductDetailPage";
+import { ProductListPage } from "../pageobjects/ProductListPage";
+import { SortingPage } from "../pageobjects/SortingPage";
+
+type AppFixtures = {
+  cartPage: CartPage;
+  checkoutCompletePage: CheckoutCompletePage;
+  loginPage: LoginPage;
+  overviewPage: OverviewPage;
+  productDetailPage: ProductDetailPage;
+  productListPage: ProductListPage;
+  sortingPage: SortingPage;
+};
+
+export const test = base.extend<AppFixtures>({
+  cartPage: async ({ page }, use) => {
+    await use(new CartPage(page));
+  },
+  checkoutCompletePage: async ({ page }, use) => {
+    await use(new CheckoutCompletePage(page));
+  },
+  loginPage: async ({ page }, use) => {
+    await use(new LoginPage(page));
+  },
+  overviewPage: async ({ page }, use) => {
+    await use(new OverviewPage(page));
+  },
+  productDetailPage: async ({ page }, use) => {
+    await use(new ProductDetailPage(page));
+  },
+  productListPage: async ({ page }, use) => {
+    await use(new ProductListPage(page));
+  },
+  sortingPage: async ({ page }, use) => {
+    await use(new SortingPage(page));
+  },
+});
+
+export { expect };
